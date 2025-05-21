76af17026c791631b9751bcfc938721e67799bba
Jesse-creat <antonmagetan19@gmail.com>
2025-05-21 13:38:49 +0700

    Add files via upload

---
+++ gamemanager/src/main/java/com/example/gamemanager/model/MobileGame.java
@@ -0,0 +1,34 @@
+package com.gamemanager.model;
+
+public class MobileGame extends Game {
+    private String platform; // Android, iOS
+    private boolean isFreeToPlay;
+    
+    public MobileGame() {
+        super();
+        this.device = "Android";
+    }
+    
+    public MobileGame(String nama, String type, double rating, double price, double discountRate, 
+                    String platform, boolean isFreeToPlay) {
+        super(nama, type, "Android", rating, price, discountRate);
+        this.platform = platform;
+        this.isFreeToPlay = isFreeToPlay;
+    }
+    
+    public String getPlatform() {
+        return platform;
+    }
+    
+    public void setPlatform(String platform) {
+        this.platform = platform;
+    }
+    
+    public boolean isFreeToPlay() {
+        return isFreeToPlay;
+    }
+    
+    public void setFreeToPlay(boolean isFreeToPlay) {
+        this.isFreeToPlay = isFreeToPlay;
+    }
+}
---
+++ gamemanager/src/main/java/com/example/gamemanager/model/PCGame.java
@@ -0,0 +1,34 @@
+package com.gamemanager.model;
+
+public class PCGame extends Game {
+    private String[] systemRequirements;
+    private boolean isMultiplayer;
+    
+    public PCGame() {
+        super();
+        this.device = "PC";
+    }
+    
+    public PCGame(String nama, String type, double rating, double price, double discountRate,
+                String[] systemRequirements, boolean isMultiplayer) {
+        super(nama, type, "PC", rating, price, discountRate);
+        this.systemRequirements = systemRequirements;
+        this.isMultiplayer = isMultiplayer;
+    }
+    
+    public String[] getSystemRequirements() {
+        return systemRequirements;
+    }
+    
+    public void setSystemRequirements(String[] systemRequirements) {
+        this.systemRequirements = systemRequirements;
+    }
+    
+    public boolean isMultiplayer() {
+        return isMultiplayer;
+    }
+    
+    public void setMultiplayer(boolean isMultiplayer) {
+        this.isMultiplayer = isMultiplayer;
+    }
+}
