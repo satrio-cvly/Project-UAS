f7234238cb8233f2cd898fe69285e6bdd23bffb3
Jesse-creat <antonmagetan19@gmail.com>
2025-05-23 06:39:00 +0700

    Add files via upload

---
+++ belajar.spring/gamemanager/src/main/java/com/example/gamemanager/GameManagerApplication.java
@@ -0,0 +1,12 @@
+package com.example.gamemanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GameManagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GameManagerApplication.class, args);
+    }
+}
